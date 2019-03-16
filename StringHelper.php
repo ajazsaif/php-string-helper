ac356da78c890e6bb31c01c542407b19a2835896
Ajaz Alam <48389212+ajazsaif@users.noreply.github.com>
2019-03-17 02:28:10 +0530

    Add files via upload

--- StringHelper.php
+++ StringHelper.php
@@ -153,3 +153,119 @@ if(! function_exists('reduceMultiples') )
 		return ($trim === TRUE) ? trim($str, $character) : $str;
 	}
 }
+
+// ------------------------------------------------------------------------
+
+if(! function_exists('randomString'))
+{
+	/**
+	 * Create a Random String
+	 *
+	 * Useful for generating passwords or hashes.
+	 *
+	 * @param	string	type of random string.  basic, alpha, alnum, numeric, nozero, unique, md5, encrypt and sha1
+	 * @param	int	number of characters
+	 * @return	string
+	 */
+
+	function randomString($type = 'alnum', $len = 8)
+	{
+		switch ($type) 
+		{
+			case 'basic':
+				return mt_rand();
+				break;
+			case 'alnum':
+			case 'numeric':
+			case 'nozero':
+			case 'alpha':
+
+			switch ($type)
+			{
+				case 'alpha':
+					$pool = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+					break;
+				case 'alnum':
+					$pool =	'0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+					break;
+				case 'numeric':
+					$pool =	'0123456789';
+					break;
+				case 'nozero':
+					$pool = '123456789';
+					break;
+			}
+
+			return substr(str_shuffle(str_repeat($pool, ceil($len/strlen($pool)))), 0, $len);
+			case 'unique':
+			case 'md5':
+				return md5(uniqid(mt_rand()));
+				break;
+			case 'encrypt':
+			case 'sha1':
+				return sha1(uniqid(mt_rand(), TRUE));
+		}
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if(! function_exists('incrementString'))
+{
+	/**
+	 * Add's _1 to a string or increment the ending number to allow _2, _3, etc
+	 *
+	 * @param	string	required
+	 * @param	string	What should the duplicate number be appended with
+	 * @param	string	Which number should be used for the first dupe increment
+	 * @return	string
+	 */
+
+	function incrementString($str, $separator = '_', $first = 1)
+	{
+		preg_match('/(.+)'.preg_quote($separator, '/').'([0-9]+)$/', $str, $match);
+		return isset($match[2]) ? $match[1].$separator.($match[2] + 1) : $str.$separator.$first;
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if(! function_exists('alternator'))
+{
+	/**
+	 * Alternator
+	 *
+	 * @param	string (as many parameters as needed)
+	 * @return	string
+	 */
+
+	function alternator()
+	{
+		static $i;
+
+		if(func_num_args() === 0)
+		{
+			$i = 0;
+			return '';
+		}
+		$args = func_get_args();
+		return $args[($i++ % count($args))];
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if(! function_exists('repeator'))
+{
+	/**
+	 * Repeater function
+	 * @param	string	$data	String to repeat
+	 * @param	int	$num	Number of repeats
+	 * @return	string
+	 */
+
+	function repeator($data, $num = 1)
+	{
+		return ($num > 0) ? str_repeat($data, $num) : '';  
+	}
+}
